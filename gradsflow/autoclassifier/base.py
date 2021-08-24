337d87d04da85fbe42a0fd371b42b95b2f5c07e7
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-24 08:45:33 +0530

    fix timeout

--- gradsflow/autoclassifier/base.py
+++ gradsflow/autoclassifier/base.py
@@ -20,6 +20,8 @@ class AutoClassifier(AutoModel):
         optimization_metric: Optional[str] = None,
         suggested_backbones: Union[List, str, None] = None,
         suggested_conf: Optional[dict] = None,
+        timeout: int = 600,
+        prune: bool = True,
         optuna_confs: Optional[Dict] = None,
     ):
         super().__init__(
@@ -28,6 +30,8 @@ class AutoClassifier(AutoModel):
             optimization_metric=optimization_metric,
             n_trials=n_trials,
             suggested_conf=suggested_conf,
+            timeout=timeout,
+            prune=prune,
             optuna_confs=optuna_confs,
         )
 
