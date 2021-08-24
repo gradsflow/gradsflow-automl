cf7ff81cc324bab281b56ed59f70fd9d30ced760
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-24 09:59:38 +0530

    import modules

--- gradsflow/__init__.py
+++ gradsflow/__init__.py
@@ -1,3 +1,8 @@
 """An AutoML Library made with Optuna and PyTorch Lightning"""
 
+from gradsflow import autoclassifier
+from gradsflow.automodel import AutoModel
+from gradsflow.autoclassifier.image import AutoImageClassifier
+from gradsflow.autoclassifier.text import AutoTextClassifier
+
 __version__ = "0.0.1a1"
