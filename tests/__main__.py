de3b1d8e10bf595787c129414671f4ee84220e65
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-29 09:44:33 +0530

    fix

--- tests/__main__.py
+++ tests/__main__.py
@@ -14,4 +14,4 @@
 
 from flash.core.data.utils import download_data
 
-download_data("https://pl-flash-data.s3.amazonaws.com/hymenoptera_data.zip", "./data")
+download_data("https://pl-flash-data.s3.amazonaws.com/hymenoptera_data.zip", "~/data")
