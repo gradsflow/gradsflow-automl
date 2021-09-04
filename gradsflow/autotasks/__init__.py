4e0c40a5e7650bf788e0e4bdf9d60bf1a8815030
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-03 20:23:07 -0700

    Adding Autotask feature (#54)
    * Autotask added
    
    * Update __init__.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix tests
    
    * Create test_auto.py
    
    * Update autotasks.py
    
    * Update autotasks.py
    
    * Update __init__.py
    
    * fix typo
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Aniket Maurya <theaniketmaurya@gmail.com>

--- gradsflow/autotasks/__init__.py
+++ gradsflow/autotasks/__init__.py
@@ -15,3 +15,4 @@
 from .autoclassification.image import AutoImageClassifier
 from .autoclassification.text import AutoTextClassifier
 from .autosummarization import AutoSummarization
+from .autotasks import autotasks
