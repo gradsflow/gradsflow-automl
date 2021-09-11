485db211d969710546b82ba219c083b7efd27780
Aniket Maurya <aniketmaurya@outlook.com>
2021-09-12 00:39:44 +0530

    add tests

--- gradsflow/data/ray_dataset.py
+++ gradsflow/data/ray_dataset.py
@@ -11,14 +11,14 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import List
+from typing import List, Union
 
 import ray
 from torch.utils.data import IterableDataset
 
 
 class RayDataset(IterableDataset):
-    def __init__(self, path: List[str]):
+    def __init__(self, path: Union[List[str], str]):
         self.path = path
         self.ds = ray.data.read_binary_files(path, include_paths=True)
 
