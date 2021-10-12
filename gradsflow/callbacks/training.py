72e06c781610fa0d0379d5072ec71e7293c04b27
Aniket Maurya <theaniketmaurya@gmail.com>
2021-10-12 15:57:12 +0530

    fixes (#112)

--- gradsflow/callbacks/training.py
+++ gradsflow/callbacks/training.py
@@ -16,10 +16,18 @@ from .callbacks import Callback
 
 
 class TrainEvalCallback(Callback):
+    def on_train_step_start(self):
+        self.model.optimizer.zero_grad()
+
     def on_train_step_end(self, *args, **kwargs):
+        # ----- AUTO OPTIMIZATION -----
+        outputs = kwargs["outputs"]
+        if not self.model.disable_auto_optimization:
+            self.model.backward(outputs["loss"])
+            self.model.optimizer.step()
+
         # ----- METRIC UPDATES -----
         tracker = self.model.tracker
-        outputs = kwargs["outputs"]
         loss = outputs["loss"].item()
         tracker.val.step_loss = loss
         tracker.track_loss(loss, mode="train")
@@ -34,20 +42,12 @@ class TrainEvalCallback(Callback):
         tracker.track_loss(loss, mode="val")
         tracker.track_metrics(outputs.get("metrics", {}), mode="val")
 
-    def on_train_epoch_start(
-        self,
-    ):
+    def on_train_epoch_start(self):
         self.model.train()
-
-    def on_train_epoch_end(self, *args, **kwargs):
         self.model.metrics.reset()
+        self.model.tracker.train.reset_metrics()
 
     def on_val_epoch_start(self):
         self.model.eval()
-
-    def on_val_epoch_end(self, *args, **kwargs):
         self.model.metrics.reset()
-
-    def on_epoch_end(self):
-        self.model.tracker.train.reset_metrics()
         self.model.tracker.val.reset_metrics()
