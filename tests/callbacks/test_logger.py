0f27cc57fe84721e06b5ce172a8ddcdab95f8425
Aniket Maurya <aniketmaurya@outlook.com>
2021-11-10 20:14:04 +0530

    test val_step

--- tests/callbacks/test_logger.py
+++ tests/callbacks/test_logger.py
@@ -28,7 +28,7 @@ data = image_dataset_from_directory(folder, transform=True, ray_data=False)
 
 def test_csv_logger():
     csv_logger = CSVLogger(filename="test_csv_logger.csv")
-    autodata = AutoDataset(train_dataloader=data.dataloader)
+    autodata = AutoDataset(train_dataloader=data.dataloader, val_dataloader=data.dataloader)
     model = DummyModel()
     model.compile()
     model.fit(autodata, callbacks=csv_logger)
@@ -40,7 +40,7 @@ def test_logger():
         CometCallback()
 
     comet = CometCallback(offline=True)
-    autodata = AutoDataset(train_dataloader=data.dataloader)
+    autodata = AutoDataset(train_dataloader=data.dataloader, val_dataloader=data.dataloader)
     model = DummyModel()
     model.compile()
     model.fit(autodata, callbacks=[comet])
