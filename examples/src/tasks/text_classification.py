77c027a8f7f58b21e0c386ac08d5030ce03f46f9
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-18 20:40:08 +0530

    update

---
+++ examples/src/tasks/text_classification.py
@@ -0,0 +1,30 @@
+from flash.core.data.utils import download_data
+from flash.text import TextClassificationData
+
+from gradsflow import AutoTextClassifier
+
+download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
+
+print("Creating datamodule...")
+datamodule = TextClassificationData.from_csv(
+    "review",
+    "sentiment",
+    train_file="data/imdb/train.csv",
+    val_file="data/imdb/valid.csv",
+)
+
+suggested_conf = dict(
+    optimizers=["adam"],
+    lr=(5e-4, 1e-3),
+)
+model = AutoTextClassifier(
+    datamodule,
+    suggested_backbones=["sgugger/tiny-distilbert-classification"],
+    suggested_conf=suggested_conf,
+    max_epochs=1,
+    optimization_metric="val_accuracy",
+    timeout=5,
+)
+
+print("AutoTextClassifier initialised!")
+model.hp_tune()
